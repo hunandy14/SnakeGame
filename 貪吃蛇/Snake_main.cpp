0b83f30eceeb6966c8825f656e5b0d1cf32fa49a
Charlotte <hunandy14@gmail.com>
2021-06-03 02:47:37 +0900

    完成

--- 貪吃蛇/Snake_main.cpp
+++ 貪吃蛇/Snake_main.cpp
@@ -8,75 +8,72 @@ using namespace std;
 #include "Unity.hpp"
 
 
-enum CanvasData {
-    Head = -1, // "●"
-    Body = -1, // "■"
-    Food = -2, // "★"
-    Empty = 0  // "　"
+class Canvas {
+public:
+	Canvas() {}
+
+	static void drawSnake(const deque<Coor>& snk_list) {
+		for (size_t i = 0; i < snk_list.size(); i++) {
+			auto&& c = snk_list[i];
+			if (i == 0) {
+				ConsoleUnity::out("●", c.x, c.y);
+			} else {
+				ConsoleUnity::out("■", c.x, c.y);
+			}
+		}
+	}
+	static void out(string str, const Coor& c) {
+		ConsoleUnity::out(str.c_str(), c.x, c.y);
+	}
+private:
+	unsigned short width = 0;
+	unsigned short height = 0;
 };
-
-
-
 
 class SnakeGame {
 public:
-    SnakeGame() {
+	SnakeGame() {
 
-    }
-    void init() {
+	}
+	void init() {
 		width = 88;
 		height = 28;
-        ConsoleUnity::init("CHG", width, height);
-        Coor start(16, 8);
-        for (int i = 5; i >= 0; i--) {
-            Coor coor(start.x + (i<<1), start.y);
-            snakecoor.emplace_back(coor);
-        }
-        Canvas::drawSnake(snakecoor);
+		ConsoleUnity::init("CHG", width, height);
+		Coor start(16, 8);
+		for (int i = 5; i >= 0; i--) {
+			Coor coor(start.x + (i << 1), start.y);
+			snakecoor.emplace_back(coor);
+		}
+		Canvas::drawSnake(snakecoor);
 
 
 		food.set(40, 8);
 		ConsoleUnity::out("★", food.x, food.y);
-    }
+	}
 
 	void getNewFood() {
-		while ((food = Coor::randCoor(width-1, height-2)).x % 2) {
-
-		}
-
-
-		Canvas::out("★", food);
+		do {
+			food = Coor::randCoor(0, width-1, 1, height-2);
+		} while (food.x % 2);
 
 		string s = "[" + to_string(food.x) + ", " + to_string(food.y) + "]";
-		ConsoleUnity::out(s.c_str(), 0, height-1);
+		ConsoleUnity::out(s.c_str(), 0, height - 1);
 	}
 
 public:
-	//enum Dire {W=119, A=97, S=115, D=100};
-	Coor degdir(){
-		Coor nexthead{snakecoor[0].x, snakecoor[0].y};
-
+	Coor degdir(Coor nexthead) {
 		static char key = 'd';// 這 static 最初第一次的時候預設是往右邊跑
-
-		if (_kbhit())
-		{
+		if (_kbhit()) {
 			char temp = _getch();
-
-			if (temp=='w' or temp=='a' or temp=='s' or temp=='d')
-			{
-				if (
-					(key == 'w' && temp != 's') || (key == 's' && temp != 'w') ||
-					(key == 'a' && temp != 'd') || (key == 'd' && temp != 'a')
-					)
+			if (temp == 'w' or temp == 'a' or temp == 's' or temp == 'd') {
+				if ((key == 'w' && temp != 's') || (key == 's' && temp != 'w') ||
+					(key == 'a' && temp != 'd') || (key == 'd' && temp != 'a'))
 				{
 					key = temp;
 				}
 			}
 		}
-
-
-		switch (key)
-		{
+		switch (key) {
 		case 'd':
 			nexthead.x += 2;
 			break;
@@ -89,111 +86,84 @@ public:
 		case 's':
 			nexthead.y += 1;
 		}
-
-		//cout << nexthead << endl;
 		return nexthead;
 	}
 
-
-	//游戏结束时需要做的事情
-	void finmatt(const int score) {
-		system("cls");
-		ConsoleUnity::out("游戏结束", 40, 14);
-		string s = "得分"+to_string(score);
-		ConsoleUnity::out(s.c_str(), 40, 16);
-		exit(0);
-	}
-
 	//游戏结束的情况
-	void finishgame(const int score) {
-		//撞墙情况
-		if (snakecoor[0].x >= width || snakecoor[0].x < 0 || 
-			snakecoor[0].y >= height || snakecoor[0].y < 0)
-		{
-			finmatt(score);
+	void checkGameOver(const int& score) {
+		bool gameOver = 0;
+		//撞墙
+		if (snakecoor[0].x >= width or snakecoor[0].x < 0 or
+			snakecoor[0].y >= height or snakecoor[0].y < 0) {
+			gameOver = 1;
 		}
-
-		//撞到自己情况
-		for (int i = 1; i < snakecoor.size(); i++)
+		//撞自己
+		for (int i = 1; i < snakecoor.size(); i++) {
 			if (snakecoor[0] == snakecoor[i]) {
-				finmatt(score);
+				gameOver = 1;
 			}
+		}
+		// 遊戲結束
+		if (gameOver) {
+			system("cls");
+			ConsoleUnity::out("游戏结束", 40, 14);
+			ConsoleUnity::out("得分" + to_string(score), 40, 16);
+			exit(0);
+		}
 	}
-
-
 
 
 
 	void move() {
-		Coor nexthead = degdir();
+		Canvas::out("得分:" + to_string(score), Coor{ 0, 0 });
+
+		Coor nexthead = degdir(snakecoor[0]);
+
 		snakecoor.push_front(nexthead);
-		Canvas::out("得分:"+to_string(score), Coor{0, 0});//每次移动都在左上角刷新得分
-		finishgame(score);
-		
+		checkGameOver(score);
+
+		Canvas::out("●", snakecoor[0]);
+		Canvas::out("■", snakecoor[1]);
 
 
-
-
-		if (snakecoor[0] == food)	//蛇头与食物重合
-		{
-			Canvas::out("●", snakecoor[0]);//吃到食物时因为直接返回此次移动没有输出蛇身，会少输出一次蛇
-											//所以在这里补上蛇移动时需要输出的字符
-			Canvas::out("■", snakecoor[1]);
-
-			score++;			 //吃到食物得分+1
-
-			auto a= food;
-			getNewFood();//如果蛇头坐标和食物坐标重合则重新产生一个食物
-			return;				 //直接结束本次移动
+		// 吃到食物
+		bool getFood=0;
+		if (snakecoor[0] == food) {
+			score++;
+			getFood=1;
+			getNewFood();
 		}
-
-		//遍历容器，判断食物与蛇身是否重合并输出整条蛇
-		for (int i = 0; i < snakecoor.size(); i++)
-		{
-			if (!i)								//头部输出圆形否则输出方块
-				Canvas::out("●", snakecoor[i]);
-			else
-				Canvas::out("■", snakecoor[i]);
-
-			//如果食物刷新到了蛇身上，则重新产生一个食物
-			if (snakecoor[i] == food){
+		//檢查食物是不是在蛇身上
+		for (int i = 0; i < snakecoor.size(); i++) {
+			if (snakecoor[i] == food) {
 				getNewFood();
 			}
 		}
+		Canvas::out("★", food);
 
-
-
-
-
-		Coor c=snakecoor.back();
-		ConsoleUnity::out("　", c.x, c.y);
-		snakecoor.pop_back();
-		//Canvas::drawSnake(snakecoor);
-	}
-
-	void printHead() {
-		cout << snakecoor[0];
+		if (!getFood) {
+			Canvas::out("　", snakecoor.back());
+			snakecoor.pop_back();
+		}
 	}
 
 private:
-    deque<Coor> snakecoor;
-	unsigned short width  = 0;
+	deque<Coor> snakecoor;
+	unsigned short width = 0;
 	unsigned short height = 0;
 	Coor food;
 	int score = 0;
 };
 //====================================================================================
 int main(int argc, char const* argv[]) {
-    SnakeGame game;
-    game.init();
-
-	
+	SnakeGame game;
+	game.init();
 
 	while (true) {
 		game.move();
 		Sleep(100);
 	}
-    //cout << _getch() << endl;
-    return 0;
+
+	return 0;
 }
 //====================================================================================
--- 貪吃蛇/Unity.hpp
+++ 貪吃蛇/Unity.hpp
@@ -37,56 +37,36 @@ public:
 };
 
 
-
 class Coor {
 public:
-	Coor(): x(0), y(0) {};
+	Coor(){}
 	Coor(const int& x, const int& y): x(x), y(y) {}
+public:
+	void set(const int& x, const int& y) {
+		this->x = x, this->y = y;
+	}
 	friend bool operator==(const Coor& lhs, const Coor& rhs){
 		bool cmp=false;
 		if (lhs.x == rhs.x and lhs.y == rhs.y) {cmp=true;}
 		return cmp;
 	}
-public:
-	void set(const int& x, const int& y) {
-		this->x = x;
-		this->y = y;
+	friend std::ostream& operator<<(std::ostream& os, const Coor& obj) {
+		os << "[" << obj.x << ",  " << obj.y << "]";
+		return os;
 	}
 public:
-	static Coor randCoor(int x_max, int y_max) {
-		return Coor(rand(0, x_max), rand(1, y_max));
+	static Coor randCoor(int x_min, int x_max, int y_min, int y_max) {
+		return Coor(rand(x_min, x_max), rand(y_min, y_max));
 	}
+private:
 	static int rand(int min, int max) {
 		static mt19937 gen{random_device{}()};
 		uniform_int_distribution<int> distribution{min, --max};
 		return distribution(gen);
 	}
 public:
-	friend std::ostream& operator<<(std::ostream& os, const Coor& obj) {
-		os << "[" << obj.x << ",  " << obj.y << "]";
-		return os;
-	}
-public:
-	int x;
-	int y;
+	int x = 0;
+	int y = 0;
 };
 
 
-class Canvas {
-public:
-	Canvas() {}
-
-	static void drawSnake(const deque<Coor>& snk_list) {
-		for (size_t i = 0; i < snk_list.size(); i++) {
-			auto&& c = snk_list[i];
-			if (i==0) {
-				ConsoleUnity::out("●", c.x, c.y);
-			} else {
-				ConsoleUnity::out("■", c.x, c.y);
-			}
-		}
-	}
-	static void out(string str, const Coor& c) {
-		ConsoleUnity::out(str.c_str(), c.x, c.y);
-	}
-};
